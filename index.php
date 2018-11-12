4727effe2c156ee821be795b9ee54cec1f944415
vincent87720 <vincent87720@yahoo.com.tw>
2018-11-12 12:22:15 +0800

    fix login bug

--- index.php
+++ index.php
@@ -12,12 +12,6 @@
         <!--Bootstrap CSS-->
         <link rel="stylesheet" href="css/bootstrap.css">
         <!--Bootstrap CSS-->
-
-        <!--Bootstrap JAVA-->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <!--Bootstrap JAVA-->
     </head>
     <body>
         <!-- CheckLogin -->
@@ -55,14 +49,14 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="col-sm-4 ml-auto mr-auto">
-                            <form method="post" name="login" action="./php/checklogin.php">
+                            <form method="post" action="php/checklogin.php">
                                 <div class="form-group">
                                     <label for="username">Username</label>
-                                    <input type="text" class="form-control" id="username" placeholder="帳號">
+                                    <input type="text" class="form-control" name="username "id="username" placeholder="帳號">
                                 </div>
                                 <div class="form-group">
                                     <label for="password">Password</label>
-                                    <input type="password" class="form-control" id="password" placeholder="密碼">
+                                    <input type="password" class="form-control" name="password" id="password" placeholder="密碼">
                                 </div>
 
                                 <!-- CheckLogin -->
@@ -74,7 +68,7 @@
                                 ?>
                                 <!-- CheckLogin -->
                                 <div class="col-sm-12 text-center">
-                                    <button type="submit" class="btn btn-default">Submit</button>                                    
+                                    <button type="submit" class="btn btn-default">Login</button>                                    
                                 </div>
                             </form>
                         </div>
--- php/checklogin.php
+++ php/checklogin.php
@@ -1,9 +1,30 @@
 ﻿<?php
 session_start();
-require_once 'connect.php';
-$username=$_POST["username"];
+//require_once 'connect.php';
+$host='127.0.0.1';
+$username='root';
+$password='iamadmin';
+$database='testdb';
 
-$sql="SELECT `密碼` FROM `user` WHERE `帳號` ='{$username}'";
+//宣告一個Link變數，連結結果會帶入link中
+$link=mysqli_connect($host,$username,$password,$database);
+if($link)
+{
+    //若傳回True，代表已建立連線
+    //設定編碼為UTF-8
+    mysqli_query($link,"SET NAMES utf8");
+}
+else
+{
+    //連線失敗，mysql_connect_error()顯示錯誤訊息
+    echo '無法連線mysql資料庫:<br/>'.mysql_connect_error();
+}
+
+
+if(isset($_POST['username'])){ $input_username = $_POST['username']; }
+$input_password=$_POST["password"];
+echo $input_password;
+$sql="SELECT `password` FROM `users` WHERE `username` like '$input_username'";
 $result=mysqli_query($link,$sql);
 
 if(mysqli_num_rows($result)!=0)
@@ -11,27 +32,28 @@ if(mysqli_num_rows($result)!=0)
     //若mysqli_num_rows()的值不等於0，代表資料庫中已經存在該帳號，則將密碼指定給變數$db_password
     $db_username=$_POST['username'];
     $db_password=mysqli_fetch_array($result);
+    echo $db_password;
 }
-else
-{
-    header('Location: ../index.php?msg=帳號不存在，請核對您的帳號是否正確');
-}
+// else
+// {
+//     header('Location: ../index.php?msg=帳號不存在，請核對您的帳號是否正確');
+// }
 
-if(isset($_POST['username']) && isset($_POST['password']))
-{
-    if($_POST['username']==$db_username && $_POST['password']==$db_password)
-    {
-        $_SESSION['is_login']=true;
-        header('Location: ../backend.php');
-    }
-    else
-    {
-        $_SESSION['is_login']=FALSE;
-        header('Location: ../index.php?msg=登入失敗，請核對您的帳號或密碼是否正確');
-    }
-}
-else
-{
-    header('Location: ../index.php?msg=請輸入使用者帳號及密碼');
-}
+// if(isset($_POST['username']) && isset($_POST['password']))
+// {
+//     if($_POST['username']==$db_username && $_POST['password']==$db_password)
+//     {
+//         $_SESSION['is_login']=true;
+//         header('Location: ../backend.php');
+//     }
+//     else
+//     {
+//         $_SESSION['is_login']=FALSE;
+//         header('Location: ../index.php?msg=登入失敗，請核對您的帳號或密碼是否正確');
+//     }
+// }
+// else
+// {
+//     header('Location: ../index.php?msg=請輸入使用者帳號及密碼');
+// }
 ?>
