c69d0143801f47f74b53f6b33655d954f2cfd65d
vincent87720 <vincent87720@yahoo.com.tw>
2018-11-19 16:36:05 +0800

    backend.php init

--- backend.php
+++ backend.php
@@ -3,6 +3,25 @@
     <head>
     </head>
     <body>
-        <p>welcome to backend!!</p>
+        <div class="topbar">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12 text-right">
+                        <br>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-secondary" onclick="location.href='index.php'">登出</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <embed src="./image/barrier.svg" style="display:block; width:70px; height:70px; margin:auto;"/>
+                        <br>
+                        <h2 class="text-center">自動化農場監測</h2>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
